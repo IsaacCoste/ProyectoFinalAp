a586d46b9cfb7e63da58915a4950202f65b1d125
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-07-15 00:13:11 -0400

    Add project files.

---
+++ ProyectoFinalAp/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoFinalAp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
