560c494d3920103a08da1d794b2bc1487cb126bd
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-07-27 16:36:15 -0400

    Elimine la llabes del namespace

--- ProyectoFinalAp/Data/ApplicationDbContext.cs
+++ ProyectoFinalAp/Data/ApplicationDbContext.cs
@@ -1,9 +1,8 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ProyectoFinalAp.Data
+namespace ProyectoFinalAp.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
 }
