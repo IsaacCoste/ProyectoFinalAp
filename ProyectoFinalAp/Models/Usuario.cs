952e0b8e9224a671ed26a60b67ad5c3fdd5bf7d7
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-08-01 19:47:29 -0400

    Cree el modelo de Usuario

---
+++ ProyectoFinalAp/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp.Models;
+
+public class Usuario
+{
+    [Key]
+    public int UsuarioId { get; set; }
+    [ForeignKey("AspNetUser")]
+    public string UserId { get; set; } = string.Empty;
+
+    [ForeignKey("AspNetRole")]
+    public string RolId { get; set; } = null!;
+    public string Nombre { get; set; } = string.Empty;
+    public DateTime FechaRegistro { get; set; }
+    [ForeignKey("UsuarioId")]
+    public virtual ICollection<Cuenta> Cuentas { get; set; } = new List<Cuenta>();
+    [ForeignKey("UsuarioId")]
+    public virtual ICollection<Presupuesto> Presupuestos { get; set; } = new List<Presupuesto>();
+}
