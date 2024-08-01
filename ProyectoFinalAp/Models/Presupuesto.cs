d5521c1e81eb788525ae344b569c5ad4a1ff62f0
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-08-01 19:46:17 -0400

    Cree el modelo de Presupuesto

---
+++ ProyectoFinalAp/Models/Presupuesto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp.Models;
+
+public class Presupuesto
+{
+    [Key]
+    public int PresupuestoId { get; set; }
+    [ForeignKey("Usuario")]
+    public int UsuarioId { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    [Range(0.1, 200000000, ErrorMessage = "El rango del monto debe estar entre 0.1 y 200000000.")]
+    public float MontoAsignado { get; set; }
+    public DateTime FechaInicio { get; set; } = DateTime.Now;
+    public DateTime FechaFin { get; set; }
+    public float TotalGastos { get; set; }
+    public virtual Usuario? Usuario { get; set; }
+    [ForeignKey("PresupuestoId")]
+    public virtual ICollection<DetalleGastoPresupuesto> DetallesGastosPresupuesto { get; set; } = new List<DetalleGastoPresupuesto>();
+}
