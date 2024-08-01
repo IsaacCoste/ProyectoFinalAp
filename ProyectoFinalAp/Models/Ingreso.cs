7392ae881e577d07895956aa2b8b1be50424e8dc
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-08-01 19:45:20 -0400

    Cree el modelo de Ingreso

---
+++ ProyectoFinalAp/Models/Ingreso.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp.Models;
+
+public class Ingreso
+{
+    [Key]
+    public int IngresoId { get; set; }
+    [ForeignKey("Cuenta")]
+    public int CuentaId { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    [Range(0.1, 200000000, ErrorMessage = "El rango del monto debe estar entre 0.1 y 200000000.")]
+    public float Monto { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.Now;
+    public virtual Cuenta? Cuenta { get; set; } 
+}
