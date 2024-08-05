94d1dbafed7cec1ea6e8dd55e05f08b33f256dd0
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-08-05 19:54:38 -0400

    Corregi algunas cosas

--- ProyectoFinalAp/Models/Usuarios.cs
+++ ProyectoFinalAp/Models/Usuarios.cs
@@ -11,9 +11,9 @@ public class Usuarios
     public string UserId { get; set; } = string.Empty;
 
     [ForeignKey("AspNetRole")]
-    public string RolId { get; set; } = null!;
+    public string RolId { get; set; } = string.Empty;
     public string Nombre { get; set; } = string.Empty;
-    public DateTime FechaRegistro { get; set; }
+    public DateTime FechaRegistro { get; set; } = DateTime.Now;
     [ForeignKey("UsuarioId")]
     public virtual ICollection<Transacciones> Transacciones { get; set; } = new List<Transacciones>();
     [ForeignKey("UsuarioId")]
