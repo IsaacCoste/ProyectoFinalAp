b42d33eeb698fcb4b32d66a89277f8c543a50811
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-08-04 22:34:31 -0400

    Agregando Email al modelo

--- ProyectoFinalAp/Models/Usuario.cs
+++ ProyectoFinalAp/Models/Usuario.cs
@@ -13,6 +13,7 @@ public class Usuario
     [ForeignKey("AspNetRole")]
     public string RolId { get; set; } = null!;
     public string Nombre { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public DateTime FechaRegistro { get; set; }
     [ForeignKey("UsuarioId")]
     public virtual ICollection<Cuenta> Cuentas { get; set; } = new List<Cuenta>();
