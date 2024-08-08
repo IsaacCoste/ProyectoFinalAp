88ba7e3622f6247d7be93627d828adf25f0e7d10
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-08-08 08:57:19 -0400

    Inyectando service

--- ProyectoFinalAp/Program.cs
+++ ProyectoFinalAp/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
 builder.Services.AddScoped<UsuariosService>();
+builder.Services.AddScoped<CategoriasService>();
 builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddAuthentication(options =>
