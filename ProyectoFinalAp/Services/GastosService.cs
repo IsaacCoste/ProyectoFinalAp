62d7ab1d6bbd8f678a65f0959e7d2b25e738fe3c
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-08-10 12:55:22 -0400

    Creando service de gastos

---
+++ ProyectoFinalAp/Services/GastosService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAp.Data;
+using ProyectoFinalAp.Models;
+using System.Linq.Expressions;
+
+namespace ProyectoFinalAp.Services;
+
+public class GastosService(ApplicationDbContext contexto)
+{
+    private readonly ApplicationDbContext _contexto = contexto;
+
+    private async Task<bool> Insertar(Gastos gasto)
+    {
+        _contexto.Gastos.Add(gasto);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+    private async Task<bool> Modificar(Gastos gasto)
+    {
+        _contexto.Gastos.Update(gasto);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+    public async Task<bool> Existe(int gastoId)
+    {
+        return await _contexto.Gastos
+            .AnyAsync(g => g.GastoId == gastoId);
+    }
+    public async Task<bool> Existe(int gastoId, string? concepto)
+    {
+        return await _contexto.Gastos
+            .AnyAsync(g => g.GastoId != gastoId
+            && g.Concepto.Equals(concepto));
+    }
+    public async Task<bool> Guardar(Gastos gasto)
+    {
+        if (!await Existe(gasto.GastoId))
+            return await Insertar(gasto);
+        else
+            return await Modificar(gasto);
+    }
+    public async Task<bool> Eliminar(Gastos gasto)
+    {
+        var cantidad = await _contexto.Gastos
+            .Where(g => g.GastoId == gasto.GastoId)
+            .ExecuteDeleteAsync();
+        return cantidad > 0;
+    }
+    public async Task<Gastos?> Buscar(int gastoId)
+    {
+        return await _contexto.Gastos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(g => g.GastoId == gastoId);
+    }
+    public async Task<List<Gastos>> Listar(Expression<Func<Gastos, bool>> criterio)
+    {
+        return await _contexto.Gastos
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
