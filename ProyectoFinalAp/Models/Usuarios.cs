891658c67c45ad56f003a6acfda973bd7fa58d39
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-08-10 13:46:36 -0400

    Eliminando ICollection de Usuarios

--- ProyectoFinalAp/Models/Usuarios.cs
+++ ProyectoFinalAp/Models/Usuarios.cs
@@ -14,6 +14,4 @@ public class Usuarios
     public string RolId { get; set; } = string.Empty;
     public string Nombre { get; set; } = string.Empty;
     public DateTime FechaRegistro { get; set; } = DateTime.Now;
-    [ForeignKey("UsuarioId")]
-    public virtual ICollection<Presupuestos> Presupuestos { get; set; } = new List<Presupuestos>();
 }
